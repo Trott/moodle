57b6e5eb381d07e2e1fdaa450bf12dfbf3e8e832
Rich Trott <rtrott@gmail.com>
2020-09-17 19:54:36 -0700

    MDL-69727: Incorrect machine learning backend PHP config information

--- lib/mlbackend/php/lang/en/mlbackend_php.php
+++ lib/mlbackend/php/lang/en/mlbackend_php.php
@@ -22,7 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['datasetsizelimited'] = 'Only part of the dataset has been evaluated due to its size. Set $CFG->mlbackend_php_no_memory_limit if you are confident that your system can cope with a {$a} dataset.';
+$string['datasetsizelimited'] = 'Only part of the dataset has been evaluated due to its size. Set $CFG->mlbackend_php_no_evaluation_limits if you are confident that your system can cope with a {$a} dataset.';
 $string['errorcantloadmodel'] = 'Model file {$a} does not exist. The model should been trained before using it to predict.';
 $string['errorlowscore'] = 'The evaluated model prediction accuracy is not very high, so some predictions may not be accurate. Model score = {$a->score}, minimum score = {$a->minscore}';
 $string['errornotenoughdata'] = 'There is not enough data to evaluate this model using the provided analysis interval.';
